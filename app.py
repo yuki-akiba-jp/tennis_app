b0dd150ce31c9fe86737895224cac9a81e52a997
yuki-akiba-jp <rabuafe@gmail.com>
2022-06-16 11:14:50 +0900

    check db

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # model-exam/app.py
 import os
 from flask import Flask
+from sqlalchemy import create_engine
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__name__))
@@ -8,4 +9,4 @@ basedir = os.path.abspath(os.path.dirname(__name__))
 app.config['SECRET_KEY'] = 'sec key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-db_uri = os.environ.get('DATABASE_URL') or "postgresql://localhost/data"
+engine = create_engine(os.environ.get('DATABASE_URL') or 'sqlite:///' + basedir,convert_unicode=True,echo=True)
