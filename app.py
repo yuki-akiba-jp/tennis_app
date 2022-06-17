707de56100dde9b0ed3002a300c5b8bc1463a762
yuki-akiba-jp <rabuafe@gmail.com>
2022-06-17 17:34:59 +0900

    init

--- app.py
+++ app.py
@@ -4,10 +4,8 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
-basedir = os.path.abspath(os.path.dirname(__name__))
-
 app.config['SECRET_KEY'] = 'sec key'
-db_url = os.environ.get('DATABASE_URL')
+db_url = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
 # or 'postgresql://postgres:postgres@localhost/data'
 app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 
