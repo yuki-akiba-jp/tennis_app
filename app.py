1f056c7e475be249472affc591b454b659867363
yuki-akiba-jp <rabuafe@gmail.com>
2022-06-17 19:56:31 +0900

    init

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 # model-exam/app.py
 import os
 
-import psycopg2
 from flask import Flask
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'sec key'
-db_url = os.environ.get('DATABASE_URL')
-
-app.config['SQLALCHEMY_DATABASE_URI'] = db_url
-# app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql://postgres:postgres@localhost/data'
+try:
+    db_url = os.getenv('DATABASE_URL')
+    db_url = db_url.replace("postgres://", "postgresql://")
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url
+except:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/data'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
