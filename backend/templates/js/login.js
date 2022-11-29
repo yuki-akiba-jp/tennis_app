df578735e792435f628d01c33bd9f1b7e78ed19c
yuki-akiba-jp <rabuafe@gmail.com>
2022-11-30 01:29:19 +0900

    ini

--- backend/templates/js/login.js
+++ backend/templates/js/login.js
@@ -2,8 +2,8 @@ const loginBtn = document.getElementById("loginBtn");
 const username = document.getElementById("username").value;
 const password = document.getElementById("password").value;
 
-loginBtn.addEventListener("click", () => {
-  if (!username || !password) {
-    alert("input username or password");
-  }
-});
+// loginBtn.addEventListener("click", () => {
+//   if (!username || !password) {
+//     alert("input username or password");
+//   }
+// });
